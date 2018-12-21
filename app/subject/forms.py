21f4e752e961c41d096a484a6e54b1b87302c608
德崇 吴 <214514867@qq.com>
2018-12-21 14:24:59 +0800

    增加了评论功能

--- app/subject/forms.py
+++ app/subject/forms.py
@@ -6,10 +6,8 @@ from wtforms.validators import DataRequired, Length, Email, Regexp, EqualTo
 
 class CreateCommentForm(FlaskForm):
     title = TextAreaField('标题', validators=[DataRequired(), Length(1, 32)], render_kw={'rows': '1'})
-    comment = TextAreaField('评论', validators=[DataRequired(), Length(1, 256)])
-    submit = SubmitField('提交')
-    back = SubmitField('返回', render_kw={
-        'onclick': "javascript:history.back(1);"})
+    comment = TextAreaField('评论', validators=[DataRequired(), Length(1, 512)])
+    submit = SubmitField('提交', render_kw={'id': 'submit'})
 
     def __init__(self, *args, **kwargs):
         super(CreateCommentForm, self).__init__(*args, **kwargs)
