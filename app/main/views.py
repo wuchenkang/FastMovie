f166ad7c0f57b128bc11a081186518463078c118
James Brown <wck593462925@outlook.com>
2019-06-05 22:04:55 +0800

    Update home page.

--- app/main/views.py
+++ app/main/views.py
@@ -7,4 +7,4 @@ from . import main
 @main.route('/')
 def index():
     movies = Movie.query.order_by(Movie.total_score / Movie.total_rating).limit(6).all()
-    return render_template('index.html', movies=movies, num=len(movies), range=range, base64=base64, )
+    return render_template('index.html', movies=movies, num=len(movies), range=range, base64=base64, round=round)
